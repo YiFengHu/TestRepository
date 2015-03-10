e8ad2de589da83642b4fcc67ccefd38cdc00c262
YiFengHu <yuiop741802006@gamil.com>
2015-03-10 23:29:33 +0800

    [DB] Refactor Repository

--- src/com/example/moneybook/db/DetailRepository.java
+++ src/com/example/moneybook/db/DetailRepository.java
@@ -1,34 +1,28 @@
 package com.example.moneybook.db;
 
-import android.database.Cursor;
-
 public class DetailRepository {
 	
 	protected static final String DATABASE_NAME = "moneybook.db";
 	protected static final String DATABASE_TABLE = "Detail";
 
-	protected static final String KEY_ROWID = "_id";
-	protected static final String KEY_SORT = "sort";
-	protected static final String KEY_CATEGORY = "category";
-	protected static final String KEY_DATE = "date";
-	protected static final String KEY_PRICE = "price";
-	protected static final String KEY_REMARK = "remark";
-	protected static final String KEY_NOTE = "note";
-	protected static final String KEY_TIME_CREATED = "created";
-	protected static final String KEY_TIME_MODIFIED = "modified";
+	protected static final String ID = "ID";
+	protected static final String IO = "io";
+	protected static final String CATEGORY = "category";
+	protected static final String DATE = "date";
+	protected static final String PRICE = "price";
+	protected static final String REMARK = "remark";
+	protected static final String TIME = "time";
 	
 	protected static String createDBSyntax(){
 		String createDBSytax = 
 			 "CREATE TABLE " + DATABASE_TABLE +"("
-			 + KEY_ROWID + " INTEGER PRIMARY KEY, "
-			 + KEY_SORT + " TEXT NOT NULL, "
-			 + KEY_CATEGORY + " TEXT NOT NULL, "
-			 + KEY_DATE + " INT NOT NULL, "
-			 + KEY_PRICE + " INT NOT NULL, "
-			 + KEY_REMARK + " TEXT NOT NULL, "
-			 + KEY_NOTE + " TEXT NOT NULL, "
-			 + KEY_TIME_CREATED + " TIMESTAMP, "
-			 + KEY_TIME_MODIFIED + " TIMESTAMP"
+			 + ID + " INTEGER PRIMARY KEY, "
+			 + IO + " TEXT NOT NULL, "
+			 + CATEGORY + " TEXT NOT NULL, "
+			 + DATE + " INT NOT NULL, "
+			 + PRICE + " INT NOT NULL, "
+			 + REMARK + " TEXT NOT NULL, "
+			 + TIME + " TIMESTAMP, "
 			 +");";
 	
 		return createDBSytax;
@@ -36,39 +30,39 @@ public class DetailRepository {
 	
 	public static String getAllDetailDescSyntax(){
 		String getAllDetail = "SELECT * FROM "+DATABASE_TABLE
-				+" ODER BY "+KEY_DATE+" DESC;";
+				+" ODER BY "+DATE+" DESC;";
 		return getAllDetail;
 	}
 	
 	public static String getAllDetailAscSyntax(){
 		String allDetailSyntax = "SELECT * FROM "+DATABASE_TABLE
-				+" ODER BY "+KEY_DATE+" ASC;";
+				+" ODER BY "+DATE+" ASC;";
 		return allDetailSyntax;
 	}
 	
 	public static String getDetailFromGivenDuration(int from, int to){
 		String allDetailSyntax = "SELECT * FROM "+DATABASE_TABLE
-				+" WHERE "+KEY_DATE + " >= " + from
-				+" AND " + KEY_DATE + " < " + (to+1);
+				+" WHERE "+DATE + " >= " + from
+				+" AND " + DATE + " < " + (to+1);
 		return allDetailSyntax;
 	}
 	
 	public static String getDailyDetailSyntax(int date){
 		String dailyDetailSyntax = "SELECT * FROM "+DATABASE_TABLE
-				+" WHERE "+KEY_DATE + " >= " + date
-				+" AND " + KEY_DATE + " < " + (date+1);
+				+" WHERE "+DATE + " >= " + date
+				+" AND " + DATE + " < " + (date+1);
 		return dailyDetailSyntax;
 	}
 	
 	public static String getWeeklyDetailSyntax(int date){
 		String weeklyDetailSyntax = "SELECT * FROM "+DATABASE_TABLE
-				+" WHERE "+KEY_DATE + " BETWEEN " + date +" AND " + (date+100);
+				+" WHERE "+DATE + " BETWEEN " + date +" AND " + (date+100);
 		return weeklyDetailSyntax;
 	}
 	
 	public static String getMonthlydetail(int date){
 		String monthlyDetailSyntax = "SELECT * FROM "+DATABASE_TABLE
-				+" WHERE "+KEY_DATE + " BETWEEN " + date +" AND " + (date+100);
+				+" WHERE "+DATE + " BETWEEN " + date +" AND " + (date+100);
 		return monthlyDetailSyntax;
 	}
 }
